aece7aa2276e1dfd97de1eeca10560f5c9e45e77
kristof0425 <dombi.kristof@gmail.com>
2017-11-05 09:48:16 +0100

    feat(search-params): add array of locations

---
+++ search-parameters.js
@@ -0,0 +1,12 @@
+module.exports = {
+  searchedAreas: [
+    'Budapest XIII. kerület',
+    'Visegrádi utca XIII. kerület',
+    'Hegedús Gyula utca XIII. kerület',
+    'Radnóti Miklós utca XIII. kerület',
+    'Népfürdő utca XIII. kerület',
+  ],
+  priceMin: 25,
+  priceMax: 32,
+  squaremeterMin: 45,
+}
