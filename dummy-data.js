d743fa5bafe530aea07197574119b15c7664979b
kristof0425 <dombi.kristof@gmail.com>
2017-11-05 18:19:21 +0100

    chore(): define generated data structure

---
+++ dummy-data.js
@@ -0,0 +1,42 @@
+const dummy = {
+  searchParams: {
+    locations: [],
+    priceMin: Number,
+    priceMax: Number,
+    squaremeterMin: Number
+  },
+  reults: {
+    BudapestXIIIKerulet: [
+      {
+        title: String,
+        link: String,
+        phoneNumber: String,
+        squareMeter: Number,
+        numberOfRooms: Number,
+        misc: {
+          isRenovated: Boolean,
+          floorsOfBuilding: Number,
+          floor: Number,
+          typeOfHeating: String,
+          longDescription: String,
+        }
+      }
+    ],
+    HegedusGyulaUtcaXIIIKerulet: [
+      {
+        title: String,
+        link: String,
+        phoneNumber: String,
+        squareMeter: Number,
+        numberOfRooms: Number,
+        misc: {
+          isRenovated: Boolean,
+          floorsOfBuilding: Number,
+          floor: Number,
+          typeOfHeating: String,
+          longDescription: String,
+        }
+      }
+    ]
+  }
+}
